a056ced279d6ad0182faf5803621a80868584ff1
Marenji <danielplatt1997@gmail.com>
2022-04-17 15:53:46 +0100

    WIP

--- src/main/java/me/marenji/listeners/BlockBreakListener.java
+++ src/main/java/me/marenji/listeners/BlockBreakListener.java
@@ -3,6 +3,9 @@ package me.marenji.listeners;
 import me.marenji.TransmutePlugin;
 import me.marenji.transmutables.TransmutableManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -19,9 +22,27 @@ public class BlockBreakListener implements Listener {
     @EventHandler
     public void onBreakBlock(BlockBreakEvent event) {
         var blockBrokenType = event.getBlock().getType();
+        handleDiamondBreaking(event);
         var transmutable = TransmutableManager.getInstance().getTransmutable(blockBrokenType);
         if (transmutable == null) return;
         transmutable.transmute(event);
+    }
+
+    private void handleDiamondBreaking(BlockBreakEvent event) {
+        var blockBrokenType = event.getBlock().getType();
+        if (blockBrokenType != Material.DIAMOND_ORE) return;
+
+        var itemHeld = event.getPlayer().getInventory().getItemInMainHand();
+        if (itemHeld == null) return;
+
+        var message = ChatColor.BLUE + event.getPlayer().getName() + " mined diamond ore";
+
+        var hasSilkTouch = itemHeld.containsEnchantment(Enchantment.SILK_TOUCH);
+        if (hasSilkTouch) {
+            message = message + " with Silk Touch";
+        }
+
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 
 }
